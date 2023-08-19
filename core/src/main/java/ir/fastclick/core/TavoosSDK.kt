7e1de364d8bb5d74b609f0386f06c965b32f6087
amir <amirb938@gmail.com>
2023-08-19 22:49:28 +0330

    feat: Add function for handling Advertising ID retrieval and generation

--- core/src/main/java/ir/fastclick/core/TavoosSDK.kt
+++ core/src/main/java/ir/fastclick/core/TavoosSDK.kt
@@ -1,19 +1,39 @@
 package ir.fastclick.core
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.os.Build
 import com.google.android.gms.ads.identifier.AdvertisingIdClient
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException
 import com.google.android.gms.common.GooglePlayServicesRepairableException
 import java.io.IOException
+import java.util.UUID
 
 class TavoosSDK {
 
-    private var preRollPrefixAddress = "http://192.168.100.100:7000/services/mobile/vast/"
+    private var preRollPrefixAddress = "https://video.tavoos.net/services/mobile/vast/"
     private var secretKey: String = "1234" // Default secret key
 
     fun setSecretKey(key: String) {
         secretKey = key
+    }
+
+    fun getVastAddress(context: Context): String {
+        val advertisingId = getOrCreateAdvertisingId(context)
+        val packageName = context.packageName
+
+        val params = mapOf(
+            "device-model" to Build.MODEL,
+            "deviceBrand" to Build.BRAND,
+            "device-os" to "android",
+            "osVersion" to Build.VERSION.SDK_INT.toString(),
+            "osAdvertisingId" to advertisingId,
+            "app-package-name" to packageName,
+            "secretKey" to secretKey
+        )
+
+        val paramString = params.map { (key, value) -> "$key=$value" }.joinToString("&")
+        return "$preRollPrefixAddress?$paramString"
     }
 
     private fun getAdvertisingId(context: Context): String {
@@ -30,21 +50,35 @@ class TavoosSDK {
         }
     }
 
-    fun getVastAddress(context: Context): String {
-        val advertisingId = getAdvertisingId(context)
-        val packageName = context.packageName
-
-        val params = mapOf(
-            "device-model" to Build.MODEL,
-            "deviceBrand" to Build.BRAND,
-            "device-os" to "android",
-            "osVersion" to Build.VERSION.SDK_INT.toString(),
-            "osAdvertisingId" to advertisingId,
-            "app-package-name" to packageName,
-            "secretKey" to secretKey
-        )
-
-        val paramString = params.map { (key, value) -> "$key=$value" }.joinToString("&")
-        return "$preRollPrefixAddress?$paramString"
+    private fun generateRandomAdvertiseId(): String {
+        return UUID.randomUUID().toString()
     }
+
+    private fun saveAdvertiseIdToSharedPreferences(context: Context, advertiseId: String) {
+        val sharedPreferences: SharedPreferences =
+            context.getSharedPreferences("AdvertiseIdPrefs", Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+        editor.putString("AdvertiseId", advertiseId)
+        editor.apply()
+    }
+
+    private fun getAdvertiseIdFromSharedPreferences(context: Context): String {
+        val sharedPreferences: SharedPreferences =
+            context.getSharedPreferences("AdvertiseIdPrefs", Context.MODE_PRIVATE)
+        return sharedPreferences.getString("AdvertiseId", "") ?: ""
+    }
+
+
+    private fun getOrCreateAdvertisingId(context: Context): String {
+        var advertisingId = getAdvertisingId(context)
+        if (advertisingId.isEmpty()) {
+            advertisingId = getAdvertiseIdFromSharedPreferences(context)
+            if (advertisingId.isEmpty()) {
+                advertisingId = generateRandomAdvertiseId()
+                saveAdvertiseIdToSharedPreferences(context, advertisingId)
+            }
+        }
+        return advertisingId
+    }
+
 }
